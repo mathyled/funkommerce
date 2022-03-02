1a40f18557ca2266fea23dbe545661b58f615801
Your Name <Luiscabezas_@outlook.com>
2022-03-02 11:04:39 -0300

    add divs for landing

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -4,7 +4,9 @@ import styles from "../styles/Landing.module.css";
 export default function Landing(){
     return(
         <div>
-            <h1 className={styles.h1} >Landing Pagee!!</h1>
+            <div></div>
+            <h1 className={styles.h1}>Landing Pagee!!</h1>
+            <div></div>
         </div>
     )
 };
