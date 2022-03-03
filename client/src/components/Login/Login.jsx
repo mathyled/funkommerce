df64927229d21c84c08a8ea12b8ff50021220423
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-03-03 08:18:08 -0500

    change(Login), add (Button, Input : component)

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,20 +1,38 @@
 import Modal from '../componentsReusable/Modal';
+import Input from '../componentsReusable/Input';
+import Button from '../componentsReusable/Button';
 import S from './Login.module.css';
 
  const Login=()=>{
 
     return (
-      <Modal buttonText='REGISTER'>
-        <form className={S.login}>
-          <h3>CREATE ACCOUNT</h3> 
-          <div className={S.inputGroup}>
-            <input type="text" name="name" />
-            <input type="password" name="password" />
-            <input type="email" name="email" />
-          </div>
-          <button>Submit</button>
-        </form>
+      <Modal buttonText="REGISTER">
+        <main className={S.containerForm}>
+          <form className={S.login}>
+            <h3>CREATE ACCOUNT</h3>
+            <div className={S.inputGroup}>
+              <Span text="Name" />
+              <Input type="text" name="name" />
+              <Span text="Last-Name" />
+              <Input type="text" name="lastName" />
+              <Span text="Password" />
+              <Input type="password" name="password" />
+              <Span text="Email" />
+              <Input type="email" name="email" />
+            </div>
+            <Button>Submit</Button>
+          </form>
+        </main>
       </Modal>
     );
 }
+
+const Span=({text})=>{
+  return (
+    <span className={S.spanText}>
+      {text}
+    </span>);
+}
+
+
 export default Login;
---
+++ client/src/components/componentsReusable/Input.jsx
@@ -0,0 +1,11 @@
+import S from './Modal.module.css';
+
+const Input =()=>{
+
+    return(
+        <input className={S.input}/>
+    )
+
+}
+
+export default Input;
