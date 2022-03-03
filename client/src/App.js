0139f1df9350a02fdba1b56644659b535f030eb4
MadMex23 <ambravo@outlook.com>
2022-03-03 16:17:49 -0300

    Add Searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import FunkoCard from './components/FunkoCard/FunkoCard';
 import Cart from "./components/Cart/Cart";
 
 import {Routes,Route} from "react-router-dom";
+import Searchbar from './components/Searchbar/Searchbar';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Landing />} />
+        <Route exact path="/" element={ <Landing />} />
+        <Route exact path="/search" element={ <Searchbar/>} />
       </Routes>
       <Cart></Cart>
       <FunkoCard></FunkoCard>
