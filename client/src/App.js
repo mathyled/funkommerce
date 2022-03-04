c9d4d650f14e41042b9c330cff34f819e7dcbd5d
unknown <mathiasimagine@gmail.com>
2022-03-03 21:49:57 -0300

    feat lenguage component

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import FunkoCard from './components/FunkoCard/FunkoCard';
 import Cart from "./components/Cart/Cart";
 
 import {Routes,Route} from "react-router-dom";
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Landing />} />
+        <Route path="/header" element={ <Header />} />
       </Routes>
       <Cart></Cart>
       <FunkoCard></FunkoCard>
