6b011fe3ed973597ee5be44ffb6f099d5fecbec8
jose1up <josealcaraz022@gmail.com>
2022-03-04 21:13:25 -0300

    table test get all product

--- api/src/controllers/cCategory.ts
+++ api/src/controllers/cCategory.ts
@@ -1,12 +1,15 @@
-import axios from 'axios'
-import { Request, Response } from 'express'
-const {product, category, features, brand} = require('../db')
+import axios from "axios";
+import { Request, Response } from "express";
+const { Category } = require("../db.ts");
 
-export const getAllCategory = async (req: Request, res: Response) =>{
-    try{
-        const allCategory = await category.findAll({})
-        res.status(200).json(allCategory)
-    }catch(error){
-        console.log(error)
-    }
-}
+
+
+export const getAllCategory = async (req: Request, res: Response) => {
+  try {
+    const allCategories = await Category.findAll({});
+
+    res.send(allCategories);
+  } catch (error) {
+    console.error(error);
+  }
+};
--- api/src/models/license/index.ts
+++ api/src/models/license/index.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const { DataTypes} = require("sequelize");
 
 module.exports = (sequelize:Sequelize) => {
-    return sequelize.define('license', {
+    return sequelize.define('License', {
         name: {
             type: DataTypes.STRING,
             allowNull: false
