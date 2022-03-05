fc3eb7117136ad2ca7b4841bc4e69e632a659b8e
charinwolf <rcharinwolf@gmail.com>
2022-03-04 21:44:59 -0300

    filters

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const GET_FUNKOS = "GET_FUNKOS"
 
+const HANDLE_CATEGORIES = "HANDLE_CATEGORIES"
+const HANDLE_BRANDS = "HANDLE_BRANDS"
+const HANDLE_LICENSE = "HANDLE_LICENSE"
+
 export function getFunkos() {
     return async (dispatch) => {
         var json = await axios.get("https://the-funko-api.herokuapp.com//api/v1/items/?page=7")
@@ -11,3 +15,25 @@ export function getFunkos() {
         })
     };
 };
+
+export function handleCategories(payload){
+    return {
+        type: HANDLE_CATEGORIES,
+        payload
+    }
+}
+
+export function handleBrands(payload){
+    return {
+        type: HANDLE_BRANDS,
+        payload
+    }
+}
+
+export function handleLicense(payload){
+    return {
+        type: HANDLE_LICENSE,
+        payload
+    }
+}
+
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,4 +1,5 @@
 import {GET_FUNKOS} from "../actions/index";
+import { HANDLE_CATEGORIES, HANDLE_BRAND, HANDLE_LICENSE } from "../actions/index";
 
 const initialState = {
     funkos:[]
@@ -11,6 +12,33 @@ export default function rootReducer(state = initialState, action) {
                 ...state,
                 funkos:action.payload
             }
+            
+            case HANDLE_CATEGORIES:
+                const allFunkos =state.funkos;
+
+                let categorieFilter = action.payload === 'ALL' ? state.funkos : allFunkos.filter((i) => i.data.atributes.category.includes(action.payload));
+                return {
+                    ...state,
+                    funko: categorieFilter
+                }
+
+                case HANDLE_BRAND:
+                    const allFunkos = state.funkos;
+
+                    let brandFilter = action.payload === 'ALL' ? state.funkos : allFunkos.filter((i) => i.data.atributes.brand.includes(action.payload));
+                    return{
+                        ...state,
+                        funko: brandFilter
+                    }
+                               
+                case HANDLE_LICENSE:
+                    const allFunkos = state.funkos;
+
+                    let licenseFilter = action.payload === 'ALL' ? state.funkos : allFunkos.filter((i) => i.data.atributes.license.includes(action.payload));
+                    return {
+                        ...state,
+                        funko: licenseFilter
+                    }
 
             default:
                 return{...state}
@@ -18,3 +46,4 @@ export default function rootReducer(state = initialState, action) {
         }
 
     }
+
