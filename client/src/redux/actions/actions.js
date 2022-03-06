51708af187e9134ca6cf5df234d8d0aa0e317a43
unknown <mathiasimagine@gmail.com>
2022-03-06 20:54:15 -0300

    feat change component

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -49,8 +49,8 @@ export const clearCart = () => (dispatch)=>{
 export const searchFunkos = (name) => {
   return async dispatch => {
     try {
-      const {data} = await axios.get(`https://the-funko-api.herokuapp.com/api/v1/items/${name}`)
-      dispatch({type: TYPES.SEARCH_FUNKOS, payload: data.data})
+      const {data} = await axios.get(`http://localhost:3001/api/producttest/s?name=${name}`)
+      dispatch({type: TYPES.SEARCH_FUNKOS, payload: data})
     }
     catch(error) {
       dispatch({type: TYPES.SEARCH_FUNKOS, payload: []})
