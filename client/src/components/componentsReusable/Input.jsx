b89959672edf646f20a152251b21497d784aa9b6
MadMex23 <ambravo@outlook.com>
2022-03-06 00:35:40 -0300

    Add validation Form

--- client/src/components/componentsReusable/Input.jsx
+++ client/src/components/componentsReusable/Input.jsx
@@ -1,9 +1,9 @@
 import S from './Generics.module.css';
 
-const Input =({placeholder,name,type})=>{
+const Input =({placeholder, name, type, value, onChange})=>{
 
     return(
-        <input className={S.input} placeholder={placeholder} name={name} type={type}/>
+        <input className={S.input} placeholder={placeholder} name={name} type={type} value={value} onChange={onChange}/>
     )
 
 }
