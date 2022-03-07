dc621f6545be376e0bffff0a65f5668922aeb958
MadMex23 <ambravo@outlook.com>
2022-03-07 09:37:47 -0300

    CSS Update

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
-import Nav from "../Nav/Nav";
+// import Nav from "../Nav/Nav";
+// import Nav2 from "../Nav/Nav2"
 import FunkoCardContainer from "../FunkoCardContainer/FunkoCardContainer";
 
 const Home = () => {
   return (
     <div>
-      <Nav />
+      {/* <Nav />
+      <Nav2/> */}
       <FunkoCardContainer />
     </div>
   );
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 import styles from "./Nav.module.css";
 import Searchbar from "../Searchbar/Searchbar";
-import { AiOutlineHome } from "react-icons/ai";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import { Link } from "react-router-dom";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
 import ItemsQuantity from "../ItemsQuantity/ItemsQuantity";
-import Order from "../Order/Order"
 import Funkommerce from "../../assets/Funkommerce.png";
-import Filters from '../Filters/Filters'
+// import { AiOutlineHome } from "react-icons/ai";
+// import Order from "../Order/Order"
+// import Filters from '../Filters/Filters'
+// import Desplegable from "../componentsReusable/Desplegable/Desplegable";
 
 
 const Nav = () => {
@@ -18,17 +19,17 @@ const Nav = () => {
    <img src={Funkommerce} alt="img-not found" className={styles.img} /> 
       </Link>
       <Searchbar />
-      < Order />
+      <div className={styles.userbtns}>
       <Login />
       <Register />
-      <Link to="/" >
+      </div>
+      {/* <Link to="/" >
         <AiOutlineHome className={styles.home} />
-      </Link>
+      </Link> */}
       <Link to="/cart" className={styles.linkToCart}>
         <ItemsQuantity />
         <MdOutlineAddShoppingCart className={styles.cartImg} />
       </Link>
-      <Filters/>
     </section>
   );
 };
