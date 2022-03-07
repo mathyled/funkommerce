ede8b20ae3fb4eb05a855c8921d6a122953d6ce9
MadMex23 <ambravo@outlook.com>
2022-03-06 21:48:15 -0300

    Changes Form

--- client/src/components/componentsReusable/Input.jsx
+++ client/src/components/componentsReusable/Input.jsx
@@ -1,9 +1,17 @@
 import S from './Generics.module.css';
 
-const Input =({placeholder, name, type, value, onChange})=>{
+const Input =({placeholder, name, type, value, step, onChange})=>{
 
     return(
-        <input className={S.input} placeholder={placeholder} name={name} type={type} value={value} onChange={onChange}/>
+        <input 
+        className={S.input} 
+        placeholder={placeholder} 
+        name={name} 
+        type={type} 
+        value={value} 
+        step={step}
+        onChange={onChange}
+        />
     )
 
 }
