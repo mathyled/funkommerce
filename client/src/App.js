51bddcdc53b7d3c6b4644d0ffefc817ee55e3135
Your Name <Luiscabezas_@outlook.com>
2022-03-09 21:13:04 -0300

    Css test

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import FunkoDetail from "./components/FunkoDetail/FunkoDetail";
+import CheckoutContainer from "./components/CheckoutContainer/CheckoutContainer";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/header" element={ <Header />} />
-        <Route  path="/detail/:id" element={ < FunkoDetail/>} />
-     
-      
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<CheckoutContainer />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/detail/:id" element={<FunkoDetail />} />
       </Routes>
     </div>
   );
