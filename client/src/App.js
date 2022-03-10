a84c1a000ecad9a6f6b3ab2a8581c2ce52eb4ce0
unknown <mathiasimagine@gmail.com>
2022-03-09 22:45:42 -0300

    add styles variables

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header"
 import Home from "./components/Home/Home";
 import FunkoDetail from "./components/FunkoDetail/FunkoDetail";
-
+import ContactUs from "./components/componentsReusable/ContactUs/ContactUs"
+import "./globalStyles/colors.css"
 function App() {
   return (
     <div className="App">
@@ -13,8 +14,8 @@ function App() {
         <Route path="/cart" element={<Cart/>} />
         <Route path="/header" element={ <Header />} />
         <Route  path="/detail/:id" element={ < FunkoDetail/>} />
-     
-      
+        <Route  path="/email" element={ < ContactUs/>} />
+        
       </Routes>
     </div>
   );
