bac05af9c02c19c0d19e4e6e5b54a11c79441585
Jose Urbani <josean.urbani@gmail.com>
2022-03-16 01:27:57 -0300

    User register & password change complete

--- api/src/routes/user/index.ts
+++ api/src/routes/user/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 const router = Router();
 
-import { signUp, signIn, profile } from "../../controllers/user";
+import { signUp, signIn, profile, confirm, forgotPassword, newPassword } from "../../controllers/user";
 import { tokenValidate } from "../../helpers/user"
 
-router.post("/signUp", signUp);
-router.post("/signIn", signIn);
-router.get("/profile", tokenValidate, profile);
-
+router.post("/signUp", signUp); //estas dos rutas solo devolveran un mensaje
+router.post("/signIn", signIn); 
+router.get("/confirm/:token", confirm); //automaticamente se confirma el usuario
+router.get("/profile", tokenValidate, profile); //ruta protegida trae todos los datos del usuario
+router.post("/newPassword", forgotPassword); //por esta ruta se manda el email para el cambio de contraseña
+router.put("/newPassword/confirm/:token", newPassword); //mandar nueva contraseña y repetirla para confirmar
 export default router;
