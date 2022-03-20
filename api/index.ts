4ae73a88a6c540fedb597acaa581bc0293006197
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-03-20 02:18:37 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import { init } from "./src/init";
 import server from "./src/app";
-const PORT = process.env.PORT || 3002
-
+const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
   init();
   console.log("server is runing ",PORT);
