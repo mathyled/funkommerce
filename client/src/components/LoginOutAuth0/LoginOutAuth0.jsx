49c8a544507eb86e388b14e6c92df5d1ec45b028
unknown <mathiasimagine@gmail.com>
2022-03-23 11:52:46 -0300

    feat LoginOutAuth0

--- client/src/components/LoginOutAuth0/LoginOutAuth0.jsx
+++ client/src/components/LoginOutAuth0/LoginOutAuth0.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import {AiOutlineLogout} from "react-icons/ai"
+import {AiOutlineLogout} from "react-icons/ai";
+import styles from "./LoginOutAuth0.module.css";
  const LoginOutAuth0 =()=>{
      const {logout} = useAuth0()
     return(
         <div>
            <button 
            onClick={()=> logout()}
+           className={styles.btn}
            ><AiOutlineLogout /></button>
         </div>
     )
